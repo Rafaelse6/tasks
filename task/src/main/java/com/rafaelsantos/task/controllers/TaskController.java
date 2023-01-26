ed4db368b8fcd5ffb461e638a19d966a8719a18a
Rafael Santos <rafa.05125@gmail.com>
2023-01-26 20:12:46 -0300

    POST, list html and redirection

--- task/src/main/java/com/rafaelsantos/task/controllers/TaskController.java
+++ task/src/main/java/com/rafaelsantos/task/controllers/TaskController.java
@@ -1,13 +1,34 @@
 package com.rafaelsantos.task.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.rafaelsantos.task.model.Task;
 
 @Controller
 public class TaskController {
 	
+	List<Task> tasks = new ArrayList<>();
+	
 	@GetMapping("/create")
-	public String create() {
+	public String home() {
 		return "create";
+	}
+	
+	@GetMapping("/list")
+	public String list() {
+		return "list";
+	}
+	
+	@PostMapping("/create")
+	public String create(Task task) {
+		Long id = tasks.size() + 1L;
+		tasks.add(new Task(id,task.getName(),task.getDate()));
+		
+		return "redirect:/list";
 	}
 }
