f7555bebd560a7e102ea29d623a0d517f37a1f38
Rafael Santos <rafa.05125@gmail.com>
2023-01-26 20:02:27 -0300

    Task, attributes mapped

---
+++ task/src/main/java/com/rafaelsantos/task/model/Task.java
@@ -0,0 +1,63 @@
+package com.rafaelsantos.task.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Task implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String name;
+	private Date date;
+	
+	public Task() {}
+
+	public Task(Long id, String name, Date date) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.date = date;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Task other = (Task) obj;
+		return Objects.equals(id, other.id);
+	}
+}
